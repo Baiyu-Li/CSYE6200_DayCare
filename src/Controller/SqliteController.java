a00fac9a4f948eac71663b17b1e38d7abe26f651
fu.yanp <49274476+fuyanp@users.noreply.github.com>
2021-12-09 01:19:58 -0500

    Add files via upload

--- src/Controller/SqliteController.java
+++ src/Controller/SqliteController.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @author fuyanping
  */
 public class SqliteController {
-    private static final String DB_URL ="jdbc:sqlite:DB/Daycare.db";
+    private static final String DB_URL ="jdbc:sqlite:Daycare.db";
     private static final String JDBC_DRIVER="Sqlite.JDBC";
     private static Connection conn=null;
     private static Statement stmt=null;
@@ -27,14 +27,20 @@ public class SqliteController {
     public static void main(String[] args){
         
     }
-    
+    public static void test(){
+        createSubjectTable();
+        createPersonTable();
+        //createTeacherTable();
+        //createStudentTable();
+        createCourseTable();
+        createCourseEnrollTable();
+        createVaccineTable();
+        //createStudentImmunizationRecordTable();
+        //createTeacherImmunizationRecordTable();
+    }
     public static void connDB(){
         try{
-            Class.forName(JDBC_DRIVER);
             conn=DriverManager.getConnection(DB_URL);
-            if(conn!=null){
-                System.out.println("Succeeded connecting to Sqlite!");
-            }
             stmt=conn.createStatement();
         }catch(Exception e){
             e.printStackTrace();
@@ -59,8 +65,7 @@ public class SqliteController {
         if(conn != null){
          try {
           conn.close();
-          System.out.println("Database connection terminated!");
-         } catch (SQLException e) {
+            } catch (SQLException e) {
           e.printStackTrace();
              }
          }
@@ -86,5 +91,184 @@ public class SqliteController {
     		closeDB();
     	}
         return ol;
+    }
+    
+    public static void createSubjectTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS Subject (SubjectID INTEGER PRIMARY KEY AUTOINCREMENT,SubjectName VARCHAR(45)) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    
+    //using person table
+    public static void createPersonTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS Person (id INTEGER PRIMARY KEY AUTOINCREMENT,fName VARCHAR(45),lName VARCHAR(45),Age INT,PIdentity VARCHAR(10)) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    /*2 tables
+    public static void createTeacherTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS Teachers (tid INTEGER PRIMARY KEY AUTOINCREMENT,fName VARCHAR(45),lName VARCHAR(45),Age INT) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    public static void createStudentTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS Students (sid INTEGER PRIMARY KEY AUTOINCREMENT,fName VARCHAR(45),lName VARCHAR(45),Age INT,SubjectID INT REFERENCES Subject(SubjectID)) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+*/
+    public static void createCourseTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS Course (CourseID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "CourseName VARCHAR(200),StartDate DATE,EndDate DATE,DayOfWeek VARCHAR(15),StartTime TIME,"
+                + "EndTime TIME,SubjectID INT REFERENCES Subject(SubjectID),tid INT REFERENCES Teachers(tid)) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    
+    public static void createCourseEnrollTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS CourseEnroll (CourseID INTEGER REFERENCES Course(CourseID),"
+                + "sid INTEGER REFERENCES Students(sid),PRIMARY KEY(CourseID,sid)) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    //need update
+    //*******************************
+    public static void createVaccineTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS Vaccine (VaccineID INTEGER PRIMARY KEY,"
+                + "sid INTEGER REFERENCES Students(sid)) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    //reference person table
+    public static void createImmunizationRecordTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS ImmunizationRecord (VaccineID INTEGER PRIMARY KEY REFERENCES Vaccine(VaccineID),"
+                + "id INTEGER REFERENCES Person(id),ImmunizedDate DATE,DoseCompleted INT) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    /*teacher and student 2 kinds*****************
+    //need update
+    //*******************************
+    public static void createStudentImmunizationRecordTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS ImmunizationRecord (VaccineID INTEGER PRIMARY KEY REFERENCES Vaccine(VaccineID),"
+                + "sid INTEGER REFERENCES Students(sid),ImmunizedDate DATE,DoseCompleted INT) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    //need update
+    //*******************************
+    public static void createTeacherImmunizationRecordTable(){
+        connDB();
+        //String dsql="drop table Subject";
+        String sql="create table IF NOT EXISTS ImmunizationRecord (VaccineID INTEGER PRIMARY KEY REFERENCES Vaccine(VaccineID),"
+                + "sid INTEGER REFERENCES Students(sid),ImmunizedDate DATE,DoseCompleted INT) ";
+        try{
+            //stmt.execute(dsql);
+            stmt.execute(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+*/
+    public static void insertSubjectTable(){
+        connDB();
+        
+        String sql="INSERT INTO Subject(SubjectName) VALUES ('csye')";
+        try{
+            stmt.executeUpdate(sql);
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
+    }
+    
+    public static void getSubjectTable(){
+        connDB();
+        String sql="SELECT * FROM Subject";
+        try{
+            rs=stmt.executeQuery(sql);
+            if(rs.next()){
+                System.out.println(rs.getInt("SubjectID"));
+            }
+        }catch(SQLException e){
+    		e.printStackTrace();
+    	}finally{
+    		closeDB();
+    	}
     }
 }
